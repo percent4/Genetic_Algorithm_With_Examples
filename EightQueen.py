bdbb5a070e24995f06511628c09a04f5f60c7c98
Jclian <33828982+percent4@users.noreply.github.com>
2018-06-04 10:17:50 +0800

    Add files via upload

--- EightQueen.py
+++ EightQueen.py
@@ -1,7 +1,16 @@
 import datetime
 import genetic_8q
 
+# 定义八皇后问题的适应度函数
+# 适应度函数为： 4*size-rowsWithQueens-colsWithQueens
+#                    -northEastDiagonalsWithQueens
+#                    -southEastDiagonalsWithQueens
+# rowsWithQueens: 皇后所在的行的集合
+# colsWithQueens: 皇后所在的列的集合
+# northEastDiagonalsWithQueens：皇后所在的副对角线的集合
+# southEastDiagonalsWithQueens：皇后所在的主对角线的集合
 def get_fitness(genes, size):
+    # 定义棋盘
     board = Board(genes, size)
     rowsWithQueens = set()
     colsWithQueens = set()
@@ -9,52 +18,63 @@ def get_fitness(genes, size):
     southEastDiagonalsWithQueens = set()
     for row in range(size):
         for col in range(size):
+            # 当某个位置为皇后时
             if board.get(row, col) == 'Q':
                 rowsWithQueens.add(row)
                 colsWithQueens.add(col)
                 northEastDiagonalsWithQueens.add(row+col)
                 southEastDiagonalsWithQueens.add(size-1-row+col)
+    # total为适应度
     total =   size - len(rowsWithQueens) \
             + size - len(colsWithQueens) \
             + size - len(northEastDiagonalsWithQueens) \
             + size - len(southEastDiagonalsWithQueens)
     return total
 
+# 输出棋盘信息， 适应度， 消耗时间
 def display(candidate, startTime, size):
     timeDiff = datetime.datetime.now() - startTime
     board = Board(candidate.Genes, size)
     board.print()
-    print("{}\t- {}\t{}".format(' '.join(map(str, candidate.Genes)), candidate.Fitness, timeDiff))
+    print("%s\t- %s\t%s"%(candidate.Genes, candidate.Fitness, timeDiff))
 
+# 定义棋盘类，参数为genes, size
 class Board:
+    # 棋盘初始化
     def __init__(self, genes, size):
         board = [['.']*size for _ in range(size)]
-        for index in range(len(genes)):
+        for index in range(size):
             row = index
             column = genes[index]
             board[row][column] = 'Q'
         self._board = board
 
+    # 获取位置为(row, column)的棋子
     def get(self, row, column):
         return self._board[column][row]
 
+    # 输出棋盘
     def print(self):
         # 0,0 prints in bottom left corner
         for i in range(len(self._board)):
             print(' '.join(self._board[i]))
 
+# 主函数，用来测试
 def main():
-    size = 8
-    geneset = [i for i in range(size)]
-    startTime = datetime.datetime.now()
+    size = 8 # 棋盘的尺寸
+    geneset = list(range(size)) # 基因库
+    startTime = datetime.datetime.now() # 开始时间
 
+    # 输出函数
     def fnDisplay(candidate):
         display(candidate, startTime, size)
 
+    # 适应度函数
     def fnGetFitness(genes):
         return get_fitness(genes, size)
 
+    # 最佳适应度为0， 即该问题的解的适应度
     optimalFitness = 0
-    genetic_8q.get_best(fnGetFitness, size, optimalFitness, geneset, fnDisplay)
+    genetic_8q.get_best(fnGetFitness, optimalFitness, geneset, fnDisplay)
 
 main()
--- genetic_8q.py
+++ genetic_8q.py
@@ -2,35 +2,26 @@ import random
 
 # 产生父代基因
 # 基因长度为length, 每个基因位来自geneSet
-def generate_parent(length, geneSet, get_fitness):
-    genes = []
-    while len(genes) < length:
-        sampleSize = min(length-len(genes), len(geneSet))
-        genes.extend(random.sample(geneSet, sampleSize))
-
+def generate_parent(geneSet, get_fitness):
+    random.shuffle(geneSet) # 随机产生父代基因
+    genes = geneSet
     fitness = get_fitness(genes) # 适应度函数
     return Chromosome(genes, fitness) # 返回染色体
 
-# 基因变异，每次只变异一个基因位
+# 基因变异，每次交换一对基因位
 def mutate(parent, geneSet, get_fitness):
-    index = random.randrange(0, len(parent.Genes))
-    childGenes = list(parent.Genes)
-    newGene, alternate = random.sample(geneSet, 2)
-    childGenes[index] = alternate if newGene == childGenes[index] else newGene
-    genes = childGenes
-    while len(set(genes)) != len(genes):
-        index = random.randrange(0, len(genes))
-        childGenes = list(genes)
-        newGene, alternate = random.sample(geneSet, 2)
-        childGenes[index] = alternate if newGene == childGenes[index] else newGene
-        genes = childGenes
+    # 产生随机交换的两个位置
+    i, j = random.sample(geneSet, 2)
+    # 交换这个位置上的基因
+    parent.Genes[i], parent.Genes[j] = parent.Genes[j], parent.Genes[i]
+    genes = parent.Genes
     fitness = get_fitness(genes) # 适应度函数
     return Chromosome(genes, fitness) # 返回染色体
 
 # 产生最优个体
 # optimalFitness：最佳适应度
-def get_best(get_fitness, targetLen, optimalFitness, geneSet, display):
-    Parent = generate_parent(targetLen, geneSet, get_fitness) # 产生父代
+def get_best(get_fitness, optimalFitness, geneSet, display):
+    Parent = generate_parent(geneSet, get_fitness) # 产生父代基因
     display(Parent) # 输出父代基因的信息
 
     # 父代基因适应度达到最优适应度时，则返回
